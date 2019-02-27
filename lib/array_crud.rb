22d0527970f4cde24b9c14dd1358b26bf10f2b19
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-02-27 01:20:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   array= ["test", "432", 5, 2, 1]
-  array.unshift("more")
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
